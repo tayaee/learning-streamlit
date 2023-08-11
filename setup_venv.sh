0e940d65afeae4332047869acefdd40fc4eb622a
tayaee <soonki.ji@gmail.com>
2023-08-11 00:09:16 +0000

    Refactoring

--- setup_venv.sh
+++ setup_venv.sh
@@ -1,5 +1,12 @@
+are_all_package_installed() {
+	python -c "import sys, pkg_resources; sys.exit(0 if all(pkg_resources.require(line.strip()) for line in open('requirements.txt')) else 1)"
+}
+
 [ ! -d venv ] && python -m venv venv || echo "DEBUG Found venv"
 source venv/bin/activate
 [ -f requirements.txt ] && {
-	python -c "import sys, pkg_resources; sys.exit(0 if all(pkg_resources.require(line.strip()) for line in open('requirements.txt')) else 1)" && echo "DEBUG All packages in requirements.txt are already installed." || pip install -r requirements.txt;
+	 are_all_package_installed && echo "DEBUG All packages in requirements.txt are installed." || {
+		echo "DEBUG pip install -r requirements.txt"
+		pip install -r requirements.txt
+	 }
 } || echo "No requirements.txt"
